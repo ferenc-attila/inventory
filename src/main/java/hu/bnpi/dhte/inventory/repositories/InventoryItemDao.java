d114e075e2a7a593490eeaca7d4277ca1646bdfa
ferenc-attila <attila.ferenc.80@gmail.com>
2022-05-04 18:23:43 +0200

    create try catch blocks in DAO class's methods to close entityManager

--- src/main/java/hu/bnpi/dhte/inventory/repositories/InventoryItemDao.java
+++ src/main/java/hu/bnpi/dhte/inventory/repositories/InventoryItemDao.java
@@ -59,22 +59,28 @@ public class InventoryItemDao {
 
     public Optional<InventoryItem> findInventoryItemById(long id) {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Optional<InventoryItem> result;
         try {
-            return Optional.ofNullable(entityManager.find(InventoryItem.class, id));
+            result = Optional.ofNullable(entityManager.find(InventoryItem.class, id));
         } finally {
             entityManager.close();
         }
+        return result;
     }
 
     public Optional<InventoryItem> findInventoryItemByInventoryId(String inventoryId) {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        Optional<InventoryItem> result = entityManager.createQuery("select i from InventoryItem i where i.inventoryId = :inventoryId",
-                        InventoryItem.class)
-                .setParameter("inventoryId", inventoryId)
-                .getResultList()
-                .stream()
-                .findFirst();
-        entityManager.close();
+        Optional<InventoryItem> result;
+        try {
+            result = entityManager.createQuery("select i from InventoryItem i where i.inventoryId = :inventoryId",
+                            InventoryItem.class)
+                    .setParameter("inventoryId", inventoryId)
+                    .getResultList()
+                    .stream()
+                    .findFirst();
+        } finally {
+            entityManager.close();
+        }
         return result;
     }
 
--- src/main/java/hu/bnpi/dhte/inventory/repositories/ResponsibleDao.java
+++ src/main/java/hu/bnpi/dhte/inventory/repositories/ResponsibleDao.java
@@ -19,83 +19,114 @@ public class ResponsibleDao {
 
     public Optional<Employee> saveEmployee(Employee employee) {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        entityManager.persist(employee);
-        entityManager.getTransaction().commit();
-        entityManager.close();
-        return Optional.of(employee);
+        Optional<Employee> result;
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(employee);
+            entityManager.getTransaction().commit();
+            result = Optional.of(employee);
+        } finally {
+            entityManager.close();
+        }
+        return result;
     }
 
     public Optional<Employee> findEmployeeById(long id) {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        Employee result = entityManager.find(Employee.class, id);
-        entityManager.close();
-        return Optional.ofNullable(result);
+        Optional<Employee> result;
+        try {
+            Employee employee = entityManager.find(Employee.class, id);
+            result = Optional.ofNullable(employee);
+        } finally {
+            entityManager.close();
+        }
+        return result;
     }
 
     public Optional<Department> saveDepartment(Department department) {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        entityManager.persist(department);
-        entityManager.getTransaction().commit();
-        entityManager.close();
-        return Optional.of(department);
+        Optional<Department> result;
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(department);
+            entityManager.getTransaction().commit();
+            result = Optional.of(department);
+        } finally {
+            entityManager.close();
+        }
+        return result;
     }
 
     public Optional<Department> findDepartmentById(long id) {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        Department result = entityManager.find(Department.class, id);
-        entityManager.close();
-        return Optional.ofNullable(result);
+        Optional<Department> result;
+        try {
+            Department department = entityManager.find(Department.class, id);
+            result = Optional.ofNullable(department);
+        } finally {
+            entityManager.close();
+        }
+        return result;
     }
 
     public Optional<Responsible> findResponsibleById(long id) {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        Responsible result = entityManager.find(Responsible.class, id);
-        entityManager.close();
-        return Optional.ofNullable(result);
+        Optional<Responsible> result;
+        try {
+            Responsible responsible = entityManager.find(Responsible.class, id);
+            result = Optional.ofNullable(responsible);
+        } finally {
+            entityManager.close();
+        }
+        return result;
     }
 
     public List<Employee> findAllEmployees() {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        List<Employee> result = entityManager.createQuery(
-                "select e from Employee e",
-                Employee.class)
-                .getResultList();
-        entityManager.close();
-        return result;
+        try {
+            return entityManager.createQuery(
+                            "select e from Employee e",
+                            Employee.class)
+                    .getResultList();
+        } finally {
+            entityManager.close();
+        }
     }
 
     public List<Department> findAllDepartments() {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        List<Department> result = entityManager.createQuery(
-                "select d from Department d",
-                Department.class)
-                .getResultList();
-        entityManager.close();
-        return result;
+        try {
+            return entityManager.createQuery("select d from Department d",
+                            Department.class)
+                    .getResultList();
+        } finally {
+            entityManager.close();
+        }
     }
 
     public List<Responsible> findAllResponsible() {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        List<Responsible> result = entityManager.createQuery(
-                "select r from Responsible r",
-                Responsible.class)
-                .getResultList();
-        entityManager.close();
-        return result;
+        try {
+            return entityManager.createQuery("select r from Responsible r",
+                            Responsible.class)
+                    .getResultList();
+        } finally {
+            entityManager.close();
+        }
     }
 
     public Optional<Employee> findEmployeeByEmail(String email) {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        Optional<Employee> result = entityManager.createQuery(
-                "select e from Employee e where e.email = :email",
-                Employee.class)
-                .setParameter("email", email)
-                .getResultList()
-                .stream()
-                .findFirst();
-        entityManager.close();
-        return result;
+        try {
+            return entityManager.createQuery(
+                            "select e from Employee e where e.email = :email",
+                            Employee.class)
+                    .setParameter("email", email)
+                    .getResultList()
+                    .stream()
+                    .findFirst();
+        } finally {
+            entityManager.close();
+        }
     }
 }
